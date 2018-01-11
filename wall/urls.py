c0cc8677946c4f4ae27242c4fbbf17d74a5577e5
ievgenburdin <ievgen.burdin@gmail.com>
2018-01-11 02:14:41 +0200

    Create inf scrolling

--- wall/urls.py
+++ wall/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
-from wall.views import Wall, MessageCreate, CommentCreate
+from wall.views import Wall, MessageCreate, CommentCreate, NextPage
 
 
 urlpatterns = [
     url(r'^$', Wall.as_view(), name='wall'),
     url(r'^message/create/$', MessageCreate.as_view(), name='message_create'),
     url(r'^comment/create/$', CommentCreate.as_view(), name='comment_create'),
+    url(r'^next/$', NextPage.as_view(), name='next_page'),
     ]
 
 
--- wall/views.py
+++ wall/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, FormView
+from django.shortcuts import render
+from django.views.generic import ListView, FormView, View
 from django.core.paginator import Paginator
 from wall.models import Message, Comment
 from wall.forms import MessageForm, CommentForm
@@ -7,16 +8,16 @@ from wall.forms import MessageForm, CommentForm
 class Wall(ListView):
     template_name = 'wall/message_list.html'
     queryset = Message.objects.all().order_by('created')
-    paginate_by = 5
+    paginate_by = 1
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        queryset=[]
+        queryset = []
         messages = Message.objects.all().order_by('-created')
         paginator = Paginator(messages, self.paginate_by)
         page = paginator.page(self.request.GET.get('page', 1))
         for message in page:
-            msg_dict = {}
+            msg_dict = dict()
             msg_dict['message'] = message
             comment = Comment.objects.filter(message=message)
             msg_dict['comment'] = comment
@@ -47,3 +48,24 @@ class CommentCreate(FormView):
         if form.is_valid():
             form.save(commit=True)
         return super().form_valid(form)
+
+
+class NextPage(View):
+    paginate_by = 1
+
+    def post(self, request):
+        num_page = int(request.POST.get('page', 1))
+        queryset = []
+        messages = Message.objects.all().order_by('-created')
+        paginator = Paginator(messages, self.paginate_by)
+        page = paginator.page(num_page)
+        for message in page:
+            msg_dict = dict()
+            msg_dict['message'] = message
+            comment = Comment.objects.filter(message=message)
+            msg_dict['comment'] = comment
+            queryset.append(msg_dict)
+        context = dict()
+        context['messages'] = queryset
+        context['page'] = page
+        return render(request, 'wall/message_block.html', context)
